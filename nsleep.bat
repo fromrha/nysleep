0aac8ca191f7035d5843088341c86ebd824f0dc9
Rahman Hanafi <80160891+fromrha@users.noreply.github.com>
2026-04-23 10:48:45 +0700

    feat: default nsleep to show help menu instead of exiting on empty arguments

--- nsleep.bat
+++ nsleep.bat
@@ -1,7 +1,6 @@
 @echo off
 if "%~1"=="" (
-    echo Error: 'nsleep' is a quick command exclusively for argument parsing.
-    echo Please provide arguments [e.g., nsleep -s -t 20m] or use 'nythsleep' for the interactive menu.
-    exit /b 1
+    python -X utf8 "%~dp0src\nythsleep.py" -h
+    exit /b 0
 )
 python -X utf8 "%~dp0src\nythsleep.py" %*
